0d4bf01b8568f38454f16e8f1de1e31751afa652
Andrew Lee <andrew@alee14.me>
2025-05-04 16:08:36 -0400

    Using environment variables for database, removed unnecessary things on the start script

--- server/start.sh
+++ server/start.sh
@@ -4,12 +4,6 @@ if [ ! -e "./vars/db-initiated" ]; then
     sleep 5
     echo "y" | dotnet exec FSO.Server.Core.dll db-init
     touch vars/db-initiated
-else
-#while [ $? -ne 2 ]; do
-    #mono watchdog.exe run --core
-    #dotnet exec FSO.Server.Core.dll run
-#done
-#    dotnet exec watchdog.exe run --core
-    dotnet exec FSO.Server.Core.dll run
-    sleep 5
 fi
+
+dotnet exec FSO.Server.Core.dll run
